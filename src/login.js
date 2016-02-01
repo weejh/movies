278e14ac75fd14e170e7eb23dc63b017f4b91a94
weejh <wee_jh@yahoo.com>
2016-02-01 18:42:50 +0800

    login/logout

--- src/login.js
+++ src/login.js
@@ -5,9 +5,6 @@ import { inject, Aurelia } from 'aurelia-framework';
 @inject(Aurelia)
 export class Login {
 
-  // username = '';
-  // password = '';
-  // error = '';
   lock = new Auth0Lock('NGW5BkUSyZSeAoPy0yC8RJMapLG4aXVT', 'weejh.auth0.com');
 
   constructor(aurelia) {
@@ -15,41 +12,21 @@ export class Login {
     // Once we have access to the Aurelia object, we can use it from
     // within our login function to set the new root view model to
     // our main App on succesful login.
-    console.log('login.js');
+    // console.log('login.js');
 
     window.localStorage.clear();
 
     this.login = () => {
-      if (window.localStorage.getItem('profile')) {
-        console.log('got user');
-        aurelia.setRoot('app');
-      } else {
-        console.log('no user');
-        this.loginAuth0()
-        this.error = 'Please enter a username and password.';
-        aurelia.setRoot('app');
-      }
+      this.lock.show((err, profile, token) => {
+        if(err) {
+          console.log(err);
+        }
+        else {
+          localStorage.setItem('profile', JSON.stringify(profile));
+          localStorage.setItem('id_token', token);
+          aurelia.setRoot('app');
+        }
+      });
     }
   }
-
-
-    loginAuth0() {
-        console.log('test login');
-
-        this.lock.show((err, profile, token) => {
-          if(err) {
-            console.log(err);
-          }
-          else {
-            // console.log('login');
-            // console.log('start in login ' + this.isAuthenticated );
-            localStorage.setItem('profile', JSON.stringify(profile));
-            localStorage.setItem('id_token', token);
-          // console.log(JSON.parse(window.localStorage.getItem('profile')).nickname)
-            this.username = JSON.parse(window.localStorage.getItem('profile')).nickname
-            this.isAuthenticated = true;
-          }
-        });
-
-      }
 }
