1ec59a3bfa6fa5d5d7aa6e65081003cf66f9d984
weejh <wee_jh@yahoo.com>
2016-02-02 23:10:45 +0800

    updated

--- build/tasks/bundle.js
+++ build/tasks/bundle.js
@@ -1,18 +1,44 @@
 var gulp = require('gulp');
-var bundler = require('aurelia-bundler');
-var bundles = require('../bundles.json');
+var bundle = require('aurelia-bundler').bundle;
 
 var config = {
   force: true,
-  baseURL: '.',
-  configPath: './config.js',
-  bundles: bundles.bundles
+  baseURL: '.',                   // baseURL of the application
+  configPath: './config.js',      // config.js file. Must be within `baseURL`
+  bundles: {
+    "dist/app-build": {           // bundle name/path. Must be within `baseURL`. Final path is: `baseURL/dist/app-build.js`.
+      includes: [
+        '[*.js]',
+        '*.html!text',
+        '*.css!text',
+      ],
+      options: {
+        inject: true,
+        minify: true
+      }
+    },
+    "dist/vendor-build": {
+      includes: [
+        'aurelia-bootstrapper',
+        'aurelia-fetch-client',
+        'aurelia-router',
+        'aurelia-animator-css',
+        'github:aurelia/templating-binding',
+        'github:aurelia/templating-resources',
+        'github:aurelia/templating-router',
+        'github:aurelia/loader-default',
+        'github:aurelia/history-browser',
+        'github:aurelia/logging-console',
+        'bootstrap/css/bootstrap.css!text'
+      ],
+      options: {
+        inject: true,
+        minify: true
+      }
+    }
+  }
 };
 
-gulp.task('bundle', ['unbundle', 'build'], function() {
-  return bundler.bundle(config);
-});
-
-gulp.task('unbundle', function() {
-  return bundler.unbundle(config);
+gulp.task('bundle', function() {
+  return bundle(config);
 });
