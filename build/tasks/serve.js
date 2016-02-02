b1f5c35cf10487832f25f04e7985dd93f144710c
weejh <wee_jh@yahoo.com>
2016-02-02 14:25:08 +0800

    updated package.json

--- build/tasks/serve.js
+++ build/tasks/serve.js
@@ -8,7 +8,7 @@ gulp.task('serve', ['build'], function(done) {
   browserSync({
     online: false,
     open: false,
-    port: 9000,
+    port: process.env.PORT || 9000,
     server: {
       baseDir: ['.'],
       middleware: function(req, res, next) {
